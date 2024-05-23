eca84289eddf0174496c53740097f226cf1a177d
Laxman879 <laxman.a@brewcode.co>
2024-05-23 16:03:45 +0530

    footer fixed

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from "react";
 import Navbar from "../Navbar";
-import Footer from "../footer";
+import Footer from "../Footer";
 
 interface LayoutProps {
   children: ReactNode;
