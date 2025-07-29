6d894e74f15607ee8c4003a92088f19cc1dbc246
Laxman879 <laxman.a@brewcode.co>
2025-07-29 14:29:48 +0530

    microsoft clarity

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,22 +4,26 @@ import { store } from "@component/store";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "swiper/css/bundle";
 import "../styles/index.css";
 import "../styles/ResponsiveStyles.css";
+
 import Loader from "@component/components/Loader";
-import GoogleAnalytics from "@component/components/GoogleAnalytics"; 
+import GoogleAnalytics from "@component/components/GoogleAnalytics";
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { useRouter } from "next/router";
 
+import Script from "next/script"; // ✅ GTM Script
+import { GTM_ID, pageview } from "@component/lib/gtm"; // ✅ GTM ID and pageview
+
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isLoading, setIsLoading] = useState<boolean>(true); 
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const router = useRouter();
 
+  // Loader logic
   useEffect(() => {
     const handleRouteChangeStart = (url: string) => {
       if (url !== router.asPath) {
@@ -35,23 +39,49 @@ function MyApp({ Component, pageProps }: AppProps) {
 
     handleRouteChangeComplete();
 
-    router.events.on('routeChangeStart', handleRouteChangeStart);
-    router.events.on('routeChangeComplete', handleRouteChangeComplete);
-    router.events.on('routeChangeError', handleRouteChangeComplete);
+    router.events.on("routeChangeStart", handleRouteChangeStart);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+    router.events.on("routeChangeError", handleRouteChangeComplete);
 
     return () => {
-      router.events.off('routeChangeStart', handleRouteChangeStart);
-      router.events.off('routeChangeComplete', handleRouteChangeComplete);
-      router.events.off('routeChangeError', handleRouteChangeComplete);
+      router.events.off("routeChangeStart", handleRouteChangeStart);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+      router.events.off("routeChangeError", handleRouteChangeComplete);
     };
   }, [router]);
 
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Provider store={store}>
-        {isLoading ? <Loader /> : <Navbar isLoading/>}
-      
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+        }}
+      />
+
+      {isLoading ? <Loader /> : <Navbar isLoading />}
+
       <Component {...pageProps} />
       <GoogleAnalytics gaId="G-59FC9R45R5" />
+      <ToastContainer />
     </Provider>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,14 +2,20 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.svg" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <Html>
+        <Head />
+        <body>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
   );
 }
