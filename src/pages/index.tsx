fb7f6c5deb1364a0dda0c4ecefad31663afeb92a
kirant043 <kirant043@gmail.com>
2025-08-14 17:30:46 +0530

    text change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const Index: React.FC = () => {
     <>
       <Head>
       {/* <img src = "/images/svg/brewcode_logo_svg.svg"/> */}
-      <title>Brewcode</title>
+      <title>Brewcode Technologies</title>
       </Head>
 
       <Home />
